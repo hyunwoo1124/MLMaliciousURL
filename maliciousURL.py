62e678b517c53f8deb94119605e89aeb3a8b6421
kevin <hyunwoo777@csu.fullerton.edu>
2020-03-31 20:39:00 -0700

    made methods for each tasks, cleaner

--- maliciousURL.py
+++ maliciousURL.py
@@ -1,73 +1,137 @@
 #!/usr/bin/python3
 
-# Handles csv files
-import pandas as pd
-# array
-import numpy as np
-# Plotting graphs
-import matplotlib.pyplot as plt
+import pandas as pd                                                             # Handles csv files
+import numpy as np                                                              # array
 
-import random
+
+import matplotlib.pyplot as plt                                                 # Graph generating library
+
+import random                                                                   # importing random and regEx
 import re
 
-# sci-kit learning model to train and test split 
-from sklearn.model_selection import train_test_split
-# sci-kit learning feature with two vectorizer: TfidfVectorizer and CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-
-# importing linear model and naive bayes multinomial
-from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split                            # sci-kit learning model to train and test split 
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer    # sci-kit learning feature with two vectorizer: TfidfVectorizer and CountVectorizer
+from sklearn.linear_model import LogisticRegression                             # importing linear model and naive bayes multinomial
 from sklearn.naive_bayes import MultinomialNB
 
 # helper library to use metric functions
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report             # helper library to use metric functions
 
 #import seaborn as sns
 
 
+def csvImport():
+    # Loading the data utilizing pandas
+
+    print("#####Loading CSV Data...#####")
+    url_df = pd.read_csv("sample.csv")
+
+    # Only taking URLs stored to test_url
+    test_url = url_df['URLs'][4]
 
 
-# Loading the data utilizing pandas
+    print("\nSample of our data of {}".format(len(url_df)))
+    print(url_df.head())
 
-print("Loading CSV Data...")
-url_df = pd.read_csv("sample.csv")
+    return url_df, test_url
 
-print("\nSample of our data of {}".format(len(url_df)))
-print(url_df.head())
+def train_test(url_df):
+    # Train = 80% and test = 20%
+    test_percentage = .2
 
-# Train = 80% and test = 20%
-test_percentage = .2
+    # data must be split between training and testing the sample
+    train_df, test_df = train_test_split(url_df, test_size = test_percentage, random_state = 42)
 
-# data must be split between training and testing the sample
-train_df, test_df = train_test_split(url_df, test_size = test_percentage, random_state = 42)
+    #labels = train_df['Class']
+    #test_labels = test_df['Class']
+    
+    print("\n#####Spliting Train and Testing...##### \n")
+    print("#                                          #")
+    print("############################################")
 
-labels = train_df['Class']
-test_tables = train_df['Class']
+    # number set to show before bar graph
+    print("\nCounting splited data frames...\n")
+    print("Training Data Sample: {}".format(len(train_df)))
+    print("Testing Data Sample:  {}".format(len(test_df)))
 
-print("\n Spliting Train and Testing... \n")
+    return train_df, test_df
 
-# number set to show before bar graph
-print("\nCounting splited data frames...\n")
-print("Training Data Sample: {}".format(len(train_df)))
-print("Testing Data Sample:  {}".format(len(test_df)))
 
-# This is where we generate a bar graph...
+def train_test_graph(train_df, test_df):
+    # This is where we generate a bar graph...
 
-# getting count of actual train set and test set
-barGraphTrain = pd.value_counts(train_df['Class'])
-barGraphTest = pd.value_counts(test_df['Class'])
+    print("\n#####Generating testing and training graph#####\n")
+    # getting count of actual train set and test set
+    barGraphTrain = pd.value_counts(train_df['Class'])
+    barGraphTest = pd.value_counts(test_df['Class'])
 
-N = 2
-ind = np.arange(N)
-width = 0.35
+    N = 2
+    ind = np.arange(N)
+    width = 0.35
 
-plt.bar(ind, barGraphTrain, width, label='Train')
-plt.bar(ind + width, barGraphTest, width, label = 'Test')
+    plt.bar(ind, barGraphTrain, width, label='Train')
+    plt.bar(ind + width, barGraphTest, width, label = 'Test')
 
-plt.ylabel('Data sets')
-plt.xlabel('Training/Testing')
-plt.title('Good and Bad URL datasets')
-plt.xticks(ind + width /2, ('Train', 'Test'))
-plt.legend(loc='best')
-plt.show()
+    plt.ylabel('Data sets')
+    plt.xlabel('Training/Testing')
+    plt.title('Good and Bad URL datasets')
+    plt.xticks(ind + width /2, ('Train', 'Test'))
+    plt.legend(loc='best')
+    plt.show()
+
+
+# Implementing tokenizer for the URL
+def tokenizerURL(url):
+    """
+    This method will split the url into tokenized forms:
+    www.example.com/this
+        ex: example,this
+    (tokenizer will remove www, . , -, /)
+    """
+    #Utilizing regEX to get [-,/] from the url
+    tokens = re.split('[/-]', url)
+
+    # for loop to iterate the whole url section
+    for tok in tokens:
+        if tok.find(".") >= 0:
+            # splits the subdomains
+            dotSplit = tok.split('.')
+            # Remove top level domain .com, .edu, .gov
+            # and www. since they're too common
+            if "com" in dotSplit:
+                dotSplit.remove("com")
+            if "edu" in dotSplit:
+                dotSplit.remove("edu")
+            if "gov" in dotSplit:
+                dotSplit.remove("gov")
+            if "www" in dotSplit:
+                dotSplit.remove("www")
+            if "org" in dotSplit:
+                dotSplit.remove("org")
+            
+            tokens += dotSplit
+
+        return tokens
+
+    print ("Tokenizer in transit...\n")
+
+
+def main():
+    url_df, test_url = csvImport()
+    train_df, test_df = train_test(url_df)
+    train_test_graph(train_df,test_df)
+    
+    print("Full URL from the sample...\n")
+    print(test_url)
+
+    print("\nURL after tokenizer...\n")
+    tokenized_url = tokenizerURL(test_url)
+    print(tokenized_url)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
 
